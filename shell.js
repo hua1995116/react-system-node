84cd1b10b2fff0a7ee7fc626cee796f6df48a86d
hua1995116 <461249104@qq.com>
2018-01-30 22:24:35 +0800

    s

--- shell.js
+++ shell.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-const child = exec("publish.sh 213",  (error, stdout, stderr) => {
+const child = exec("./publish.sh",  (error, stdout, stderr) => {
   if (error) {
     throw error;
   }
